316cc9869f491171046845ea5c4c36292726afdf
MicaelChaves <micaelgpchaves@gmail.com>
2022-08-20 18:12:38 -0400

    primeira versão

---
+++ js/music-list.js
@@ -0,0 +1,97 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Harley Bird - Home",
+    artist: "Jordan Schor",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Ikson Anywhere – Ikson",
+    artist: "Audio Library",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Beauz & Jvna - Crazy",
+    artist: "Beauz & Jvna",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Hardwind - Want Me",
+    artist: "Mike Archangelo",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Jim - Sun Goes Down",
+    artist: "Jim Yosef x Roy",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Lost Sky - Vision NCS",
+    artist: "NCS Release",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Buttercup",
+    artist: "Buttercup",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Yoko",
+    artist: "Yoko",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "SALES",
+    artist: "renee",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Valasy",
+    artist: "Nürnberg",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: "Mondo",
+    artist: "Jimmy Fontana",
+    img: "music-11",
+    src: "music-11"
+  },
+  {
+    name: "Something About You",
+    artist: "Eydress & Dent May",
+    img: "music-12",
+    src: "music-12"
+  },
+  {
+    name: "The Outfield",
+    artist: "Your Love",
+    img: "music-13",
+    src: "music-13"
+  },
+  {
+    name: "Where My Rosemary Goes",
+    artist: "Love Grows",
+    img: "music-14",
+    src: "music-14"
+  },
+];
